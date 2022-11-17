d0d6c2735fc47faaaf64c226b116e058757a21c9
Uygnis <75312898+Uygnis@users.noreply.github.com>
2022-11-17 21:54:24 +0800

    Delete application directory

--- application/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from pymongo import MongoClient
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
-
-app = Flask(__name__, template_folder='templates')
-
-from application import routes
