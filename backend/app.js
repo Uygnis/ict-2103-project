150ee9c66544d6c8dae4249f57fa13f34f32514a
xunyuan <2101297@sit.singaporetech.edu.sg>
2022-11-21 14:49:47 +0800

    MySQL Connection File
    Addition of MySQL Connection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const pool = require("./config/db.js");
 
 const app = express();
 app.use(express.json());
@@ -23,3 +24,18 @@ database.once("connected", () => {
 
 const mongoRoutes = require("./mongodb/routes/routes");
 app.use("/api/mongo", mongoRoutes);
+
+app.listen(process.env.DB_PORT, () => {
+  console.log(`Server Started at ${process.env.DB_PORT}`);
+});
+
+//MYSQL DB Connection
+pool.getConnection( (err, conn) => {
+  if (err) throw err;
+  const qry = 'SELECT * FROM project.amazon';
+  conn.query(qry, (err, result) => {
+    conn.release();
+    if (err) throw err;
+    console.log(result);
+  });
+});
