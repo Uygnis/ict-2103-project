7b0ac639010af1bebb3551547920ebb05d898e3f
Jansenc08 <93838712+Jansenc08@users.noreply.github.com>
2022-11-22 15:15:35 +0800

    Created dashboard frontend
    Created dashboard for frontend website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,23 @@ import logo from "./logo.svg";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import About from './pages/About.jsx';
+import Analytics from './pages/Analytics.jsx';
+import Comment from './pages/Comment.jsx';
+import Product from './pages/Product.jsx';
+import ProductList from './pages/ProductList.jsx';
+
+
 function App() {
   const [db, setDb] = useState([]);
   const fetchData = async () => {
     try {
       const { data } = await axios.get(
-        `http://localhost:5001/api/amazon_data/get`
+        `http://localhost:5001/api/mysql/amazon_data/get`
       );
       setDb(data);
     } catch (err) {
@@ -19,22 +30,19 @@ function App() {
     console.log(db);
   }, [db.length]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
 }
 
