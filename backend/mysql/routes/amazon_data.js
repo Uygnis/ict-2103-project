e38bf81e9d469bc8291df485a4f66359ae71f586
Uygnis <vesperlsy@gmail.com>
2022-11-23 16:42:23 +0800

    Update amazon_data.js

--- backend/mysql/routes/amazon_data.js
+++ backend/mysql/routes/amazon_data.js
@@ -12,4 +12,57 @@ router.get("/get", (req, res) => {
   });
 });
 
+//localhost:5001/api/mysql/amazon_data/get/:id
+router.get("/get/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+  const qry2 = `SELECT * FROM amazon WHERE item_ID = ${id}`;
+  db.query(qry2, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/getPriceMax
+router.get("/getPriceMax", (req, res) => {
+  db.query("Select MAX(Price) FROM amazon", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/getPriceMin
+router.get("/getPriceMin", (req, res) => {
+  db.query("SELECT MIN(price) FROM amazon", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/getQty
+router.get("/getQty", (req, res) => {
+  db.query("SELECT COUNT(item_ID) FROM amazon", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/update/:id
+router.get("/update/:id", (req, res) => {
+  db.query("INSERT item_ID FROM amazon", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
 module.exports = router;
