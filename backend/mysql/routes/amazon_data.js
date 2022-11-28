90fb947183fca59edcc774181bce405111a0dc10
xunyuan <2101297@sit.singaporetech.edu.sg>
2022-11-29 00:23:31 +0800

    Update Amazon_data,js for MySQL

--- backend/mysql/routes/amazon_data.js
+++ backend/mysql/routes/amazon_data.js
@@ -22,7 +22,8 @@ router.post("/post", (req, res) => {
 
 //localhost:5001/api/mysql/amazon_data/get
 router.get("/get", (req, res) => {
-  db.query("SELECT * FROM amazondata", (err, result) => {
+  const qry1 = 'SELECT * FROM amazon, cpu_benchmark_passmark, gpu_score WHERE amazon.CPU_NAME = cpu_benchmark_passmark.cpuName AND amazon.GPU_NAME = gpu_score.Device';
+  db.query(qry1, (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -43,9 +44,22 @@ router.get("/get/:item_ID", (req, res) => {
   });
 });
 
+//localhost:5001/api/mysql/amazon_data/q=:query
+router.get("/q=:query", async (req, res) => {
+  const query = req.params.query;
+  console.log(req.params);
+  const qry3 = 'SELECT * FROM amazon WHERE Listing LIKE %${query}%';
+  db.query(qry3, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
 //localhost:5001/api/mysql/amazon_data/getPriceMax
 router.get("/getPriceMax", (req, res) => {
-  db.query("Select MAX(Price) FROM amazon", (err, result) => {
+  db.query('Select MAX(Price) FROM amazon', (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -55,7 +69,7 @@ router.get("/getPriceMax", (req, res) => {
 
 //localhost:5001/api/mysql/amazon_data/getPriceMin
 router.get("/getPriceMin", (req, res) => {
-  db.query("SELECT MIN(price) FROM amazon", (err, result) => {
+  db.query('SELECT MIN(price) FROM amazon', (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -65,7 +79,7 @@ router.get("/getPriceMin", (req, res) => {
 
 //localhost:5001/api/mysql/amazon_data/getQty
 router.get("/getQty", (req, res) => {
-  db.query("SELECT COUNT(item_ID) FROM amazon", (err, result) => {
+  db.query('SELECT COUNT(item_ID) FROM amazon', (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -76,14 +90,9 @@ router.get("/getQty", (req, res) => {
 //localhost:5001/api/mysql/amazon_data/update/:item_ID
 router.patch("/update/:item_ID", (req, res) => {
   const item_ID = req.params.item_ID;
-  // const CPU_Name = req.params.CPU_Name;
-  // const GPU_Name = req.params.GPU_Name;
-  // const ram = req.params.ram;
-  // const price = req.params.price;
-  // const Listing = req.params.Listing;
   const {CPU_Name, GPU_Name, ram, price, Listing} = req.body;
-  const qry3 = 'UPDATE amazon SET (CPU_Name, GPU_Name, ram, price, Listing) VALUES (?, ?, ?, ?, ?) WHERE item_ID = ${item_ID}';
-  db.query(qry3, (err, result) => {
+  const qry4 = 'UPDATE amazon SET (CPU_Name, GPU_Name, ram, price, Listing) VALUES (?, ?, ?, ?, ?) WHERE item_ID = ${item_ID}';
+  db.query(qry4, (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -96,13 +105,47 @@ router.patch("/update/:item_ID", (req, res) => {
 router.delete("/delete/:item_ID", (req, res) => {
   const item_ID = req.params.item_ID;
   const {CPU_Name, GPU_Name, ram, price, Listing} = req.body;
-  const qry4 = 'DELETE * FROM amazon WHERE item_ID = ${item_ID}'
-  db.query(qry4, (err, result) => {
+  const qry5 = 'DELETE * FROM amazon WHERE item_ID = ${item_ID}'
+  db.query(qry5, (err, result) => {
+    if (err) {
+     console.log(err);
+    }
+    res.send(result);
+    rconsole.log("Number of records deleted: " + result.affectedRows);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/join
+router.get("/join", (req, res) => {
+  db.query('SELECT * FROM amazon INNER JOIN cpu_benchmark_passmark ON amazon.CPU_NAME = cpu_benchmark_passmark.cpuName INNER JOIN gpu_score ON amazon.GPU_NAME = gpu_score.Device', (err, result) => {
     if (err) {
       console.log(err);
     }
     res.send(result);
-    rconsole.log("Number of records deleted: " + result.affectedRows);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/filter/:cpuMark
+router.get("/filter/:cpuMark", (req, res) => {
+  const cpuMark = req.params.cpuMark;
+  const qry6 = 'SELECT * FROM amazon INNER JOIN cpu_benchmark_passmark ON amazon.CPU_NAME = cpu_benchmark_passmark.cpuName INNER JOIN gpu_score ON amazon.GPU_NAME = gpu_score.Device WHERE cpu_benchmark_passmark.cpuMark >= ${cpuMark}';
+  db.query(qry6, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+//localhost:5001/api/mysql/amazon_data/sort
+router.get("/sort", (req, res) => {
+  const OpenCL = req.params.OpenCL
+  const qry7 = 'SELECT * FROM amazon INNER JOIN cpu_benchmark_passmark ON amazon.CPU_NAME = cpu_benchmark_passmark.cpuName INNER JOIN gpu_score ON amazon.GPU_NAME = gpu_score.Device ORDER BY gpu_Score.OpenCL DESC';
+  db.query(qry7, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
   });
 });
 
