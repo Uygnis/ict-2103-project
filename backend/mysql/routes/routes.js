6a751d633863be26f9a34bd7f2b6becda60b06c3
Jansenc08 <93838712+Jansenc08@users.noreply.github.com>
2022-11-28 19:20:04 +0800

    Implemented amazon data table with pagination

--- backend/mysql/routes/routes.js
+++ backend/mysql/routes/routes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
 // split up route handling
 router.use("/amazon_data", require("./amazon_data"));
-// router.use("/gpu_specs", require("./gpu_specs"));
-// router.use("/gpu_score", require("./gpu_score"));
-// router.use("/gpu_benchmarks", require("./gpu_benchmarks"));
-// router.use("/steam_gpu_popularity", require("./steam_gpu_popularity"));
-// router.use("/cpu_specs", require("./cpu_specs"));
-// router.use("/cpu_benchmark_cinebench", require("./cpu_benchmark_cinebench"));
-// router.use("/cpu_benchmark_passmark", require("./cpu_benchmark_passmark"));
+ router.use("/gpu_specs", require("./gpu_specs"));
+ router.use("/gpu_score", require("./gpu_score"));
+ router.use("/gpu_benchmarks", require("./gpu_benchmarks"));
+ router.use("/steam_gpu_popularity", require("./steam_gpu_popularity"));
+ router.use("/cpu_specs", require("./cpu_specs"));
+ router.use("/cpu_benchmark_cinebench", require("./cpu_benchmark_cinebench"));
+router.use("/cpu_benchmark_passmark", require("./cpu_benchmark_passmark"));
 
 module.exports = router;
